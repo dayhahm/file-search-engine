9bcf516cfe751fe0e2ae513b800e59e069101dc5
dayhahm <debbie.hahm@gmail.com>
2020-12-06 16:06:16 +0900

    fix errors in BoundedBuffer

--- BoundedBuffer.java
+++ BoundedBuffer.java
@@ -13,7 +13,7 @@ public class BoundedBuffer {
         this.size = 0;
         this.end = -1;
         this.start = 0;
-        this.buffer = new String[capacity];
+        this.buffer = new Path[capacity];
     }
 
     public boolean isEmpty() {
@@ -31,16 +31,22 @@ public class BoundedBuffer {
     public void enqueue(Path file) {
         synchronized (this) {
             // wait for a spot to open up if the buffer is full
-            while (isFull())
-                this.wait();
+            while (isFull()) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) { // replace with more appropriate catch later on
+                    System.out.println("Thread was interrupted while waiting.");
+                }
+            }
+
 
             //fill the buffer
             this.buffer[start++] = file;
-            this.count++;
+            this.size++;
 
             // the buffer was previously empty, notify all the threads
-            if (this.count == 1) {
-                this.notifyAll()
+            if (this.size == 1) {
+                this.notifyAll();
             }
         }
     }
@@ -48,16 +54,21 @@ public class BoundedBuffer {
     public Path dequeue() {
         synchronized (this) {
             // wait for an object to take out of the buffer
-            while (isEmpty())
-                this.wait();
+            while (isEmpty()) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) { // replace with more appropriate catch later on
+                    System.out.println("Thread was interrupted while waiting.");
+                }
+            }
 
             // take out and object from the buffer
             Path file = this.buffer[end--];
-            this.count--;
+            this.size--;
 
             // the buffer was previously full, notify all the threads. (is this necessary if we only have one producer thread)
-            if (count == capacity - 2) {
-                this.notifyAll;
+            if (this.size == capacity - 2) {
+                this.notifyAll();
             }
             return file;
         }
