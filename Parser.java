916a5d17f1c9a88570df419a80d529e809ed310c
dayhahm <debbie.hahm@gmail.com>
2020-12-08 19:10:08 +0900

    finish parser, and simplest ranker (mvp complete)

--- Parser.java
+++ Parser.java
@@ -1,4 +1,7 @@
 import java.nio.file.*;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Parser extends Thread {
     /*
@@ -10,17 +13,51 @@ public class Parser extends Thread {
     // this map needs to be shared by all the threads. so maybe this needs to exist in the thread control
 //    private Map<String, ADT> wordMap;   //is HashTable an implementation of Map?
     private BoundedBuffer buffer;
+    private ConcurrentHashMap<String, PriorityQueue<ADT>> wordToFileCount;
 
-    public Parser(BoundedBuffer buffer) {
+    public Parser(BoundedBuffer buffer, ConcurrentHashMap wordToFileCount) {
         super();
         this.buffer = buffer;
+        this.wordToFileCount = wordToFileCount;
+    }
+
+    public void parse(Path file) {
+        Map<String, Integer> wordCounter = new HashMap<>();
+        try {
+            List<String> lines = Files.readAllLines(file);
+            for (String line: lines) {
+                for (String word: line.split("\\s+")) {
+                    wordCounter.put(word, wordCounter.getOrDefault(word, 0) + 1);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading " + file);
+        }
+
+        synchronized(wordToFileCount) {
+            for (String word : wordCounter.keySet()) {
+                if (wordToFileCount.containsKey(word)) {
+                    PriorityQueue<ADT> q = wordToFileCount.get(word);
+                    q.offer(new ADT(file, wordCounter.get(word)));
+                } else {
+                    PriorityQueue<ADT> q = new PriorityQueue<>(new Comparator<ADT>(){
+                        @Override
+                        public int compare(ADT a1, ADT a2) {
+                            return a1.getCount() < a2.getCount() ? 1: -1;
+                        }
+                    });
+                    q.offer(new ADT(file, wordCounter.get(word)));
+                    wordToFileCount.put(word, q);
+                }
+            }
+        }
     }
 
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Path file = buffer.dequeue();
-                System.out.println(file);
+                parse(file);
             } catch (InterruptedException e) {
                 System.out.println("parser interrupted");
             }
--- ThreadRunner.java
+++ ThreadRunner.java
@@ -1,16 +1,55 @@
+import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.nio.file.*;
+
 public class ThreadRunner {
 
     public static void main(String[] args) {
         BoundedBuffer buffer = new BoundedBuffer(20);
-        Parser parser = new Parser(buffer);
-        String dir = "/Users/debbie";
+        ConcurrentHashMap<String, PriorityQueue<ADT>> wordToFileCount = new ConcurrentHashMap<>();
+        Parser p1 = new Parser(buffer, wordToFileCount);
+        Parser p2 = new Parser(buffer, wordToFileCount);
+        Parser p3 = new Parser(buffer, wordToFileCount);
+        Parser p4 = new Parser(buffer, wordToFileCount);
+        Parser p5 = new Parser(buffer, wordToFileCount);
+        String dir = "/Users/debbie/Documents/Coronavirus-Twitter-Trends";
         try {
             Traverser traverser = new Traverser(buffer, dir);
-            parser.start();
+            System.out.println("Starting up all threads");
             traverser.start();
-        } catch (Exception e) {
+            p1.start();
+            p2.start();
+            p3.start();
+            p4.start();
+            p5.start();
+
+            traverser.join();
+
+            System.out.println("Traversal has completed — parsing may be ongoing");
+
+            try {
+                Ranker ranker = new Ranker(wordToFileCount);
+                Scanner s = new Scanner(System.in);
+                System.out.println("Enter a word to search: ");
+                String word = s.nextLine();
+                while (!word.equals("quit")) {
+                    ADT[] filePairs = ranker.getTop(word);
+                    for (int i = 0; i < filePairs.length; i++) {
+                        System.out.println((i + 1) + " - " + filePairs[i].getFilename() + ":" + filePairs[i].getCount());
+                    }
+                    System.out.println("Enter a word to search: ");
+                    word = s.nextLine();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (
+                Exception e) {
             System.out.println(e);
             System.exit(1);
         }
+        System.out.println("quitting");
+        System.exit(0);
     }
 }
